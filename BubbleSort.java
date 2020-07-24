f0af4896f8f670ae3f482bd9bf8fc2bb786b26c7
yamaj <62327968+yamakaz814@users.noreply.github.com>
2020-07-24 23:52:16 +0900

    オブジェクト

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,6 +1,14 @@
 public class BubbleSort {
   public static void main(String[] args){
     int array[] = new int[]{2,9,6,7,5,10,3,4,8,1};
+    sort(array);
+    for(int x:array){
+      System.out.print(x);
+    }
+  }
+
+  public static void sort(int[] array){
+
     for(int i = 0;i < array.length - 1;i++){
       for(int j = array.length - 1; j > i; j--){
         if(array[j] < array[j - 1]){
@@ -9,10 +17,6 @@ public class BubbleSort {
           array[j - 1] = n;
         }
       }
-          }
-    
-    for(int x:array){
-      System.out.print(x);
     }
   }
-}
+  }
